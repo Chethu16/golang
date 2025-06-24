fe790ce80980ffbb114441d9720374201e838c80
Chethu16 <chethanpoojaryiruvail@gmail.com>
2025-06-24 12:07:03 +0530

    changes

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var a, b int
 	fmt.Println("Enter the number of a and b")
-	fmt.Scanln(&a,&b)
-	fmt.Println("The addition of a and b is", a+b)
+	fmt.Scanln(&a, &b)
+	fmt.Println("The addition of a and b is", a-b)
 
 }
